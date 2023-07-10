49fa81bb36be0cd69b66daa8989a4967b4181b83
KiranKumarSingh18 <kirankumarsingh789@gmail.com>
2023-07-10 22:26:34 +0530

    Update Company.java

--- Company.java
+++ Company.java
@@ -1,5 +1,42 @@
 package com.kiran.model;
 
 public class Company {
+	String companyName;
+	String loaction;
+	Department[] department;
+	
+	Company() { }
 
+	public Company(String companyName, String loaction, Department[] department) {
+		super();
+		this.companyName = companyName;
+		this.loaction = loaction;
+		this.department = department;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public String getLoaction() {
+		return loaction;
+	}
+
+	public void setLoaction(String loaction) {
+		this.loaction = loaction;
+	}
+
+	public Department[] getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department[] department) {
+		this.department = department;
+	}
+	
+	
 }
